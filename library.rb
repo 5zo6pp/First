03947b3e37c5f2dfff2a42a9f0ced7ff7d2f464a
5zo6pp <kd1220234@st.kobedenshi.ac.jp>
2016-11-22 12:18:22 +0000

    カテゴリ・QT整備

--- library.rb
+++ library.rb
@@ -7,23 +7,20 @@
       #infomation===========
       puts "1から順番に尋ねてください。"
       puts ""
-      puts "1>あいさつ"
-      puts "2>身につけるもの"
-      puts "3>食事"
-      puts "4>環境"
-      puts "5>知人"
-      puts "6>趣味"
-      puts "7>今の感情"
-      puts "8>体調"
-      puts "9>コミュニケーション"
+      puts "1>してほしいことがありますか？"
+      puts "2>お話したいことがありますか？"
+      puts ""
+      puts "tips"
+      puts "ゆっくり大きな声を心がけましょう"
+      puts ""
 
 
       #input===========================*/
       puts "当てはまる数字を入力して下さい。"
       mother_answer = gets.to_i
 
-      puts "入力された数字は #{mother_answer} ですか？"
-
+      puts "確認>入力された数字は #{mother_answer} です。"
+      puts ""
       return mother_answer
 
     end
@@ -33,28 +30,58 @@
 
     def questionTree(mother_answer)
       #--------init----------
-      i = 0
-      mqid = 0
+      i = 0 #カウンタ変数
 
-      mqeach = {1=>"mqid01.csv", 2=>"mqid02.csv",3=>"mqid03.csv",4=>"mqid04.csv",
-                5=>"mqid05.csv", 6=>"mqid06.csv",7=>"mqid07.csv",8=>"mqid08.csv",
-                9=>"mqid09.csv"}
-      cqid = Array.new
-      child_question = Array.new
+      cqid = Array.new  #質問ID
+      child_question = Array.new  #質問本文
+      mq_each = Array.new   #マザー選択
+      mq_each = {1=>"mqid01.csv", 2=>"mqid02.csv"}
 
-      #------fininit-------
+      answer = "Y"  #回答
+      last_answer_flg = 0 #フラグ
+      answer_path = Array.new #回答パス
+      answer_i = 0  #回答カウンタ変数1
+      answer_j = 1  #回答カウンタ変数2
 
-      puts "#{mother_answer}に関する質問です。"
-
-
-      CSV.foreach(mqeach[mother_answer]) do | questionid |
+      CSV.foreach("#{mq_each[mother_answer]}") do | questionid |
         cqid[i] = questionid[0]
         child_question[i] = questionid[1]
         i += 1
       end
 
-      puts cqid
+      #------fininit-------
+
+      #-----main-----------
+      puts "#{mother_answer}に関する質問です。"
       puts ""
-      puts child_question
+
+      #----mainquestion----
+      puts child_question[answer_j]
+      puts "「はい」→Yキー"
+      puts "「いいえ」→Nキー"
+      puts "を入力してください"
+      answer = gets.chop
+
+      #-----質問パス--------
+      answer_path[answer_i] = answer_i
+
+      until last_answer_flg == 1 do
+
+        if answer == "Y" then #回答がYならば
+          answer_j = answer_j*2 #配列の添字のカウンタを変更
+          puts child_question[answer_j] #質問文を表示
+          answer_path[answer_i] = answer_j  #回答パスに追加
+          answer_i += 1 #カウンタ変数をインクリメント
+          last_answer_flg = 1
+
+        elsif answer == "N"
+          answer_j = answer_j*2+1 #配列の添字のカウンタを変更
+          puts child_question[answer_j] #質問文を表示
+          answer_path[answer_i] = answer_j  #回答パスに追加
+          answer_i += 1 #カウンタ変数をインクリメント
+          last_answer_flg = 1
+
+      end
 
     end
+  end
